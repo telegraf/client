c71cd3003137bd6eea6d55581c0b5cebbf978592
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-11-15 14:40:44 +0100

    Dedupe dependencies

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,4 @@
 // deno-lint-ignore-file no-explicit-any
-// @ts-expect-error "module": "node12" bug
-import { Blob, FormData } from "formdata-node";
-// @ts-expect-error "module": "node12" bug
-import type { RequestInit } from "node-fetch";
 import type { ApiResponse, InputFileProxy as Typegram } from "@grammyjs/types";
 import type { StreamFile } from "./stream-file.js";
 
@@ -28,14 +24,20 @@ const defaultApi: Api = {
   root: new URL("https://api.telegram.org"),
 };
 
-function stringify(value: unknown) {
-  if (typeof value === "string") return value;
-  if (value instanceof Blob) return value;
-  return JSON.stringify(value);
-}
+async function serialize(payload: Record<string, any>) {
+  const [{ Blob }, { FormData }] = await Promise.all([
+    import("fetch-blob"),
+    import("formdata-polyfill/esm.min.js"),
+  ]);
+  const formData = new FormData();
 
-const attachTo = (formData: FormData) =>
-  (entry: any, index: number) => {
+  function stringify(value: unknown) {
+    if (typeof value === "string") return value;
+    if (value instanceof Blob) return value;
+    return JSON.stringify(value);
+  }
+
+  const attach = (entry: any, index: number) => {
     const result = { ...entry };
     if (entry.media instanceof Blob) {
       const id = entry.type + index;
@@ -50,11 +52,9 @@ const attachTo = (formData: FormData) =>
     return result;
   };
 
-function serialize(payload: Record<string, any>) {
-  const formData = new FormData();
   for (let [key, value] of Object.entries(payload)) {
     if (key === "media") {
-      value = value.map(attachTo(formData));
+      value = value.map(attach);
     }
     if (value != null) formData.append(key, stringify(value));
   }
@@ -82,7 +82,7 @@ export class Client {
     { signal }: CallOptions = {},
   ): Promise<ApiResponse<ReturnType<Telegram[M]>>> {
     const fetch = (await import("node-fetch")).default;
-    const body = serialize(payload);
+    const body = await serialize(payload);
     const api = this.options.api ?? defaultApi;
     const url = new URL(`./${api.mode}${this.#token}/${method}`, api.root);
     const init: RequestInit = { body, signal, method: "post" };
